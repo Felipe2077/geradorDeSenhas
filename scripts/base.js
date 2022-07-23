811b4873d2b2287abdfa6faa113b5c05e1521e3a
Felipe Batista <Felipebatista47@hotmail.com>
2022-07-23 13:49:01 -0300

    first commit

---
+++ scripts/base.js
@@ -0,0 +1,104 @@
+export function getNumbers() {
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return numbers;
+}
+
+export function getLetraMinuscula() {
+  const LetraMinuscula = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "y",
+    "x",
+    "z",
+  ];
+  return LetraMinuscula;
+}
+
+export function getLetraMaiuscula() {
+  let letraMaiuscula = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "Y",
+    "X",
+    "Z",
+  ];
+  return letraMaiuscula;
+}
+
+export function getChar() {
+  const char = [
+    '"',
+    "# ",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "]",
+    "^",
+    "_",
+    "{",
+    "|",
+    "}",
+    "~",
+  ];
+
+  return char;
+}
---
+++ scripts/script.js
@@ -0,0 +1,152 @@
+import * as base from "./base.js";
+
+//*variables
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+let passOutput = document.getElementById("password");
+let novo = [];
+let restartPass = document.getElementById("restartPass");
+let copyPass = document.getElementById("copyPass");
+let inputMaiuscula = document.getElementById("maisculas");
+let inputNumero = document.getElementById("numeros");
+let inputSimbolo = document.getElementById("simbolos");
+let checkBox = document.querySelectorAll(".pass-check input");
+let colorBar = document.getElementById("passColorBar");
+
+let getPassWord = () => {
+  let password = [];
+  if (inputMaiuscula.checked && !inputNumero.checked && !inputSimbolo.checked) {
+    //* apenas letra maiuscula
+    password = base.getLetraMinuscula().concat(base.getLetraMaiuscula());
+  } else if (
+    !inputMaiuscula.checked &&
+    inputNumero.checked &&
+    !inputSimbolo.checked
+  ) {
+    //* apenas numero
+    password = base.getLetraMinuscula().concat(base.getNumbers());
+  } else if (
+    !inputMaiuscula.checked &&
+    !inputNumero.checked &&
+    inputSimbolo.checked
+  ) {
+    //* apenas simbolo
+    password = base.getLetraMinuscula().concat(base.getChar());
+  } else if (
+    inputMaiuscula.checked &&
+    inputNumero.checked &&
+    !inputSimbolo.checked
+  ) {
+    //* maiuscula e numero
+    password = base
+      .getLetraMinuscula()
+      .concat(base.getLetraMaiuscula(), base.getNumbers());
+  } else if (
+    inputMaiuscula.checked &&
+    !inputNumero.checked &&
+    inputSimbolo.checked
+  ) {
+    //* maiuscula e simbolo
+    password = base
+      .getLetraMinuscula()
+      .concat(base.getLetraMaiuscula(), base.getChar());
+  } else if (
+    !inputMaiuscula.checked &&
+    inputNumero.checked &&
+    inputSimbolo.checked
+  ) {
+    //* numero e simbolo
+    password = base
+      .getLetraMinuscula()
+      .concat(base.getNumbers(), base.getChar());
+  } else if (
+    inputMaiuscula.checked &&
+    inputNumero.checked &&
+    inputSimbolo.checked
+  ) {
+    //* todos
+    password = base
+      .getLetraMinuscula()
+      .concat(base.getNumbers(), base.getChar(), base.getLetraMaiuscula());
+  } else {
+    password = base.getLetraMinuscula();
+  }
+  return password;
+};
+
+//* Functions
+function generatePass() {
+  novo = "";
+  for (let i = 0; i < slider.value; i++) {
+    const random = Math.floor(Math.random() * getPassWord().length);
+    novo += getPassWord()[random];
+  }
+  passOutput.setAttribute("value", (passOutput.innerHTML = novo));
+}
+
+function generateColorBar(slider) {
+  if (slider <= 6) {
+    colorBar.classList.remove("forte");
+    colorBar.classList.remove("media");
+    colorBar.classList.add("fraca");
+    colorBar.textContent = "Essa é uma senha fraca";
+  } else if (slider >= 7 && slider <= 9) {
+    colorBar.classList.remove("forte");
+    colorBar.classList.remove("fraca");
+    colorBar.classList.add("media");
+    colorBar.textContent = "Ainda falta um pouco.";
+  } else if (slider > 9 && slider <= 14) {
+    colorBar.classList.remove("media");
+    colorBar.classList.remove("fraca");
+    colorBar.classList.add("forte");
+    colorBar.textContent = "Ótimo! Essa senha é forte";
+  } else if (slider > 14) {
+    colorBar.textContent = "Você atingiu o nível máximo de segurança da senha!";
+  }
+}
+
+function printPassOnScreen() {
+  output.innerHTML = slider.value;
+  slider.oninput = function () {
+    output.innerHTML = this.value;
+    generatePass();
+    generateColorBar(slider.value);
+  };
+}
+checkBox.forEach((element) => {
+  element.addEventListener("click", () => {
+    generatePass();
+  });
+});
+
+let bannerCopy = document.querySelector(".bannerContainer");
+
+function copyToClipBoard() {
+  passOutput.select();
+  passOutput.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  passOutput.setSelectionRange(0, 0);
+}
+
+function generateBanner() {
+  bannerCopy.classList.remove("inativo");
+  bannerCopy.classList.add("ativo");
+  setTimeout(() => {
+    bannerCopy.classList.remove("ativo");
+    bannerCopy.classList.add("inativo");
+  }, 1500);
+}
+
+copyPass.addEventListener("click", copyToClipBoard);
+copyPass.addEventListener("click", generateBanner);
+
+restartPass.addEventListener("click", generatePass);
+restartPass.addEventListener("click", () => {
+  restartPass.classList.add("clique");
+  setTimeout(() => {
+    restartPass.classList.remove("clique");
+  }, 550);
+});
+
+generatePass();
+printPassOnScreen();
